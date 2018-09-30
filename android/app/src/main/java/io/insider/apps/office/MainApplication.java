df824a9d7c97c9d1a34635e64f9a6221f48212f7
gaykov <sdgaykov@gmail.com>
2018-09-30 15:26:35 +0300

    Fix Android build

--- android/app/src/main/java/io/insider/apps/office/MainApplication.java
+++ android/app/src/main/java/io/insider/apps/office/MainApplication.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.officeassistant.BuildConfig;
+import io.insider.apps.office.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;
